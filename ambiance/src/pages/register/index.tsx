3f34b69cca6bfab3544f902ca60a3acfdc8609eb
Amanda <aamandaallinger@outlook.com>
2024-01-10 14:44:31 +0100

    användare loggas in efter lyckad registrering

--- ambiance/src/pages/register/index.tsx
+++ ambiance/src/pages/register/index.tsx
@@ -1,10 +1,12 @@
 // pages/register/index.ts
-
 import React, { useState } from 'react';
 import RegisterForm, { RegistrationFormInput } from '../../components/Auth/RegisterForm'; 
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
     const RegisterPage: React.FC = () => {
-        const [isLoading, setIsLoading] = useState(false);
+      const router = useRouter();
+      const [isLoading, setIsLoading] = useState(false);
 
         const handleRegisterSubmit = async (data: RegistrationFormInput) => {
             try {
@@ -26,6 +28,22 @@ import RegisterForm, { RegistrationFormInput } from '../../components/Auth/Regis
                 // Registreringen lyckades
                 const responseData = await response.json();
                 console.log('Registration successful:', responseData);
+          
+                // Logga in användaren efter registreringen
+                const signInResponse = await signIn("credentials", {
+                  redirect: false,
+                  email: data.email, // Använd registrerade e-postadressen
+                  password: data.password, // Använd registrerat lösenord
+                });
+          
+                if (signInResponse?.error) {
+                  // Inloggningen efter registreringen misslyckades, hantera fel här
+                  console.error('Inloggning efter registrering misslyckades:', signInResponse.error);
+                } else {
+                  // Användaren är nu inloggad
+                  console.log('Användaren är inloggad efter registrering:', signInResponse);
+                  router.push('/');
+                }
               } else {
                 // Hantera fel vid registreringen
                 const errorData = await response.json();
@@ -37,7 +55,6 @@ import RegisterForm, { RegistrationFormInput } from '../../components/Auth/Regis
               setIsLoading(false);
             }
           };
-          
 
   return (
     <RegisterForm 
