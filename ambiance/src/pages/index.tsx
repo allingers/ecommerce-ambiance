ed23da03a5b145f63e8c687a6143be7871656d86
Amanda <aamandaallinger@outlook.com>
2024-01-10 09:21:08 +0100

    start up - first commit

---
+++ ambiance/src/pages/index.tsx
@@ -0,0 +1,13 @@
+// pages/index.tsx
+
+const Home: React.FC = () => {
+  return (
+      <div>
+        <h1>Welcome to My E-Commerce Store!</h1>
+        {/* Lägg till annat innehåll för startsidan här */}
+      </div>
+  );
+};
+
+export default Home;
+
