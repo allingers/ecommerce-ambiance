4243bca0757806676b919aa368a5f67ce4108990
Amanda <aamandaallinger@outlook.com>
2024-01-18 13:53:27 +0100

    Sida och komponent för att visa enskild produkt. Relaterade produkter. Rekommendationer.

--- ambiance/src/components/SingleProduct/SingleProduct.tsx
+++ ambiance/src/components/SingleProduct/SingleProduct.tsx
@@ -1,54 +1,227 @@
 // components/ProductDetail.tsx
-import React from 'react';
-import { ProductModel } from '@/models/Product';
-import { Carousel } from '@mantine/carousel';
-import classes  from './SingleProduct.module.css'
-import { Text, Center, Container, Image, SimpleGrid, Title } from '@mantine/core';
+import React from 'react'
+import { ProductModel } from '@/models/Product'
+import { Carousel } from '@mantine/carousel'
+import classes from './SingleProduct.module.css'
+import {
+	Text,
+	Center,
+	Container,
+	Image,
+	SimpleGrid,
+	Title,
+	Divider,
+	Button,
+	UnstyledButton,
+	Card,
+	Group,
+} from '@mantine/core'
+import { GoHeart } from 'react-icons/go'
+import { useCart } from '@/contexts/CartContext'
+import { BsHandbag } from 'react-icons/bs'
 
 interface ProductDetailProps {
-  product: ProductModel;
+	product: ProductModel
+	relatedProducts: ProductModel[]
+	recommendedProducts: ProductModel[]
 }
 
-const SingleProduct: React.FC<ProductDetailProps> = ({ product }) => {
-  return (
-    <> 
-    <Container>
-    <Center>
-        <SimpleGrid 
-        className={classes.wrapper}
-        cols={{ base: 1, md:2, sm:2 }}
-        spacing={{ base: 10, sm: 'xl' }}
-        >
-         <div className={classes.CarouselContainer}>
-            <Carousel
-            height={400} 
-            w={400}
-            loop
-            withIndicators
-            >
-            {product.imageUrls.map((url, index) => (
-            <Carousel.Slide key={index}>
-                <Image h={400}fit="cover" src={url} alt={`Product Image ${index + 1}`} />
-            </Carousel.Slide>
-            ))}
-            </Carousel>
-            </div>
-        <div className={classes.ProductInfo}>
-            <Text>{product.brand}</Text>
-            <Title order={2}>{product.name}</Title>
-            <Text>{product.description}</Text>
-            <Text fw={'bold'} mt={10}>{product.price} SEK</Text>
-            {/* <Text>I Lager {product.inStock} st</Text> */}
-            <Text fw={200} mt={10}> Kategori: {product.categories.main} -  Underkategori: {product.categories.sub}</Text>
+const SingleProduct: React.FC<ProductDetailProps> = ({
+	product,
+	relatedProducts,
+	recommendedProducts,
+}) => {
+	const { addToCart } = useCart()
 
-        </div>
-        </SimpleGrid>
-      </Center>
-      </Container>
+	const handleAddToCart = () => {
+		// Anropa addToCart-funktionen från useCart och skicka med produktens id och kvantitet
+		addToCart(product._id, 1, product.price) // Här används 1 som standardkvantitet, du kan anpassa den beroende på ditt behov
+		console.log(`Produkt med id ${product._id} lades till i varukorgen.`)
+	}
 
+	return (
+		<>
+			<Container>
+				<Center>
+					<SimpleGrid
+						className={classes.wrapper}
+						cols={{ base: 1, md: 2, sm: 2 }}
+						spacing={{ base: 10, sm: 'xl' }}>
+						<div className={classes.CarouselContainer}>
+							<Carousel height={400} w={400} loop withIndicators>
+								{product.imageUrls.map((url, index) => (
+									<Carousel.Slide key={index}>
+										<Image
+											h={400}
+											fit="cover"
+											src={url}
+											alt={`Product Image ${index + 1}`}
+										/>
+									</Carousel.Slide>
+								))}
+							</Carousel>
+						</div>
+						<div className={classes.ProductInfo}>
+							<Text tt="uppercase" c="dimmed" size="sm" pb={5}>
+								{product.brand}
+							</Text>
+							<Title className={classes.ProductTitle} fw={500} order={2}>
+								{product.name}
+							</Title>
+							<Text className={classes.text} pt={15}>
+								{product.description}
+							</Text>
+							<Text fw={'bold'} mt={10}>
+								{product.price} SEK
+							</Text>
+							{/* <Text>I Lager {product.inStock} st</Text> */}
+							<Text c="dimmed" size="sm" mt={10}>
+								{' '}
+								Kategori: {product.categories.main} - Underkategori:{' '}
+								{product.categories.sub}
+							</Text>
+							<div className={classes.ButtonContainer}>
+								<Button
+									className={classes.CartButton}
+									onClick={handleAddToCart}
+									variant="filled"
+									size="lg">
+									Lägg i varukorgen
+								</Button>
+								<UnstyledButton
+									className={classes.HeartButton}
+									size="lg"
+									color="palevioletred">
+									<span className={classes.HeartIconSpan}>
+										<GoHeart />
+									</span>
+									{/* <span className={classes.FilledHeartIconSpan}><GoHeartFill /></span>  */}
+								</UnstyledButton>
+							</div>
+						</div>
+					</SimpleGrid>
+				</Center>
+			</Container>
+			<Container size={'xl'}>
+				<Title className={classes.title} order={3} fw={500} ml={5}>
+					Andra köpte också:
+				</Title>
+				<SimpleGrid
+					cols={{ base: 1, sm: 2, lg: 3 }}
+					spacing={{ base: 10, sm: 'sm' }}
+					verticalSpacing={{ base: 'xs', sm: 'xl' }}>
+					{relatedProducts.map((relatedProduct) => (
+						<div
+							key={relatedProduct._id}
+							className={classes.RelatedProductContainer}>
+							<Card withBorder radius="xs" p={0} className={classes.card}>
+								<Group wrap="nowrap" gap={0}>
+									<Image
+										height={60}
+										ml={10}
+										src={relatedProduct.imageUrls[0]}
+										alt={`Related Product Image`}
+									/>
+									<div className={classes.body}>
+										<Text tt="uppercase" c="dimmed" fw={600} size="xs">
+											{relatedProduct.brand}
+										</Text>
+										<Text className={classes.ProductTitle} mt="xs" mb="xs">
+											{relatedProduct.name}
+										</Text>
+										<Group wrap="nowrap" gap="xs">
+											<Text fw={'500'}>{relatedProduct.price} SEK</Text>
+										</Group>
+									</div>
+									<div className={classes.IconContainer}>
+										<UnstyledButton className={classes.CartIconButton}>
+											<span className={classes.CartIconSpan}>
+												<BsHandbag />
+											</span>
+										</UnstyledButton>
+										<UnstyledButton className={classes.HeartIconButton}>
+											<span className={classes.HeartIconSpan}>
+												<GoHeart />
+											</span>
+											{/* <span className={classes.FilledHeartIconSpan}><GoHeartFill /></span> */}
+										</UnstyledButton>
+									</div>
+								</Group>
+							</Card>
+						</div>
+					))}
+				</SimpleGrid>
+			</Container>
+			<Divider />
+			<Container size={'xl'} className={classes.RecommendedContainer}>
+				<Title
+					className={classes.title}
+					ta={'center'}
+					order={1}
+					pt={35}
+					pb={30}
+					fw={300}>
+					Rekommenderas för dig
+				</Title>
+				<Center>
+					<SimpleGrid
+						cols={{ base: 1, sm: 2, lg: 4 }}
+						spacing={{ base: 10, sm: 'sm' }}
+						verticalSpacing={{ base: 'md', sm: 'xl' }}>
+						{recommendedProducts.map((recommendedProducts) => (
+							<div key={recommendedProducts._id}>
+								<Card
+									className={classes.RecommendedCard}
+									withBorder
+									shadow="xs"
+									radius="xs"
+									padding="lg"
+									h={300}
+									w={250.75}>
+									<Card.Section pl={15} color="#8b8989">
+										<Text fw={400} size="xs" mt="md">
+											{recommendedProducts.brand}
+										</Text>
+									</Card.Section>
+									<Card.Section className={classes.imageSection}>
+										<Image
+											height={150}
+											fit="contain"
+											mt={20}
+											src={recommendedProducts.imageUrls[0]}
+											alt={`Related Product Image`}
+										/>
+									</Card.Section>
+									<Group justify="space-between" mt="md">
+										<Card.Section pl={15} mb={10}>
+											<Text fw={450} size="lg" mt="xs">
+												{recommendedProducts.name}
+											</Text>
+											<Text fw={500} size="md">
+												{recommendedProducts.price} SEK
+											</Text>
+										</Card.Section>
+										<Card.Section className={classes.iconSection} mb={10}>
+											<Text
+												className={classes.cartIcon}
+												onClick={handleAddToCart}
+												role="button"
+												aria-label="Add to Cart">
+												<BsHandbag />
+											</Text>
+											<Text className={classes.favIcon}>
+												<GoHeart />
+											</Text>
+										</Card.Section>
+									</Group>
+								</Card>
+							</div>
+						))}
+					</SimpleGrid>
+				</Center>
+			</Container>
+		</>
+	)
+}
 
-    </>
-  );
-};
-
-export default SingleProduct;
+export default SingleProduct
--- ambiance/src/pages/product/[productId].tsx
+++ ambiance/src/pages/product/[productId].tsx
@@ -1,60 +1,71 @@
 // pages/product/[productId].tsx
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { ProductModel } from '@/models/Product';
-import SingleProduct from '@/components/SingleProduct/SingleProduct';
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { ProductModel } from '@/models/Product'
+import SingleProduct from '@/components/SingleProduct/SingleProduct'
+import { Center, Container, SimpleGrid, Title } from '@mantine/core'
 
 const SingleProductPage: React.FC = () => {
-  const router = useRouter();
-  const { productId } = router.query;
+	const router = useRouter()
+	const { productId } = router.query
+	const [relatedProducts, setRelatedProducts] = useState<ProductModel[]>([])
+	const [recommendedProducts, setrecommendedProducts] = useState<
+		ProductModel[]
+	>([])
+	const [product, setProduct] = useState<ProductModel | null>(null)
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState<string | null>(null)
 
-  const [product, setProduct] = useState<ProductModel | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+	useEffect(() => {
+		const fetchProductById = async () => {
+			try {
+				const response = await fetch(`/api/product/${productId}`)
+				if (!response.ok) {
+					throw new Error(`Error fetching product: ${response.status}`)
+				}
 
-  useEffect(() => {
-    const fetchProductById = async () => {
-      try {
-        const response = await fetch(`/api/product/${productId}`);
-        if (!response.ok) {
-          throw new Error(`Error fetching product: ${response.status}`);
-        }
+				const data = await response.json()
+				setProduct(data.product)
+				setRelatedProducts(data.relatedProducts)
+				setrecommendedProducts(data.recommendedProducts)
+				setLoading(false)
+			} catch (error) {
+				setError((error as Error).message || 'Unknown error occurred')
+				setLoading(false)
+			}
+		}
 
-        const data = await response.json();
-        setProduct(data);
-        setLoading(false);
-      } catch (error) {
-        setError((error as Error).message || 'Unknown error occurred');
-        setLoading(false);
-      }
-    };
+		if (productId) {
+			fetchProductById()
+		}
+	}, [productId])
 
-    if (productId) {
-      fetchProductById();
-    }
-  }, [productId]);
+	if (loading) {
+		return <p>Loading...</p>
+	}
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+	if (error) {
+		return <p>Error: {error}</p>
+	}
 
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
+	if (!product) {
+		return <p>Product not found</p>
+	}
 
-  if (!product) {
-    return <p>Product not found</p>;
-  }
+	return (
+		<>
+			{loading && <p>Loading...</p>}
+			{error && <p>Error: {error}</p>}
+			{product && (
+				<SingleProduct
+					key={product._id}
+					product={product}
+					relatedProducts={relatedProducts}
+					recommendedProducts={recommendedProducts}
+				/>
+			)}
+		</>
+	)
+}
 
-  return (
-   <>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error}</p>}
-      {product && (
-     <SingleProduct key={product._id} product={product}/>
-      )}
-    </>
-  );
-};
-
-export default SingleProductPage;
+export default SingleProductPage
