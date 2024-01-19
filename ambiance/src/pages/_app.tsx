df145afd96517e53abb112810694cca95cf78ba5
Amanda <aamandaallinger@outlook.com>
2024-01-19 11:40:24 +0100

    Uppdated CartDrawer styling

--- ambiance/src/pages/_app.tsx
+++ ambiance/src/pages/_app.tsx
@@ -1,30 +1,28 @@
 // _app.tsx
-import React from 'react';
-import { AppProps } from 'next/app';
-import '../styles/globals.css';
-import '@mantine/core/styles.css';
-import { MantineProvider, createTheme } from '@mantine/core';
-import Layout from '@/components/Layout';
-import { SessionProvider } from 'next-auth/react';
-import { CartProvider } from '@/contexts/CartContext';
-import '@mantine/carousel/styles.css';
+import React from 'react'
+import { AppProps } from 'next/app'
+import '../styles/globals.css'
+import '@mantine/core/styles.css'
+import { MantineProvider, createTheme } from '@mantine/core'
+import Layout from '@/components/Layout'
+import { SessionProvider } from 'next-auth/react'
+import { CartProvider } from '@/contexts/CartContext'
+import '@mantine/carousel/styles.css'
 
-const theme = createTheme({
-  /** Put your mantine theme override here */
-});
+const theme = createTheme({})
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <MantineProvider theme={theme}>
-      <SessionProvider session={pageProps.session}>
-      <CartProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      </CartProvider>
-      </SessionProvider>
-    </MantineProvider>
-  );
+	return (
+		<MantineProvider theme={theme}>
+			<SessionProvider session={pageProps.session}>
+				<CartProvider>
+					<Layout>
+						<Component {...pageProps} />
+					</Layout>
+				</CartProvider>
+			</SessionProvider>
+		</MantineProvider>
+	)
 }
 
-export default App;
+export default App
